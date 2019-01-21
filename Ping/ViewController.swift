01556d9efd932ade706d2832c301d24cf424abe7
Ryan <ryan@macbook-pro.local>
2019-01-21 22:58:26 +0000

    Initial Commit

---
+++ Ping/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ping
+//
+//  Created by Ryan Soanes on 21/01/2019.
+//  Copyright © 2019 LionStone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
