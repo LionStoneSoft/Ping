286e7e2426643deae4f87ca48be8f42a534bb834
Ryan Soanes <ryan@lionstone.app>
2019-04-05 14:22:44 +0100

    Implemented log out feature in settings view

--- Ping/Controller/UserSettings.swift
+++ Ping/Controller/UserSettings.swift
@@ -7,15 +7,43 @@
 //
 
 import UIKit
+import Firebase
 
-class UserSettings: UIViewController {
+class UserSettings: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet var settingsTable: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        settingsTable.delegate = self //sets self as delegate for table view
+        settingsTable.dataSource = self //sets self as data source for table view
+        settingsTable.register(UINib(nibName: "UserSettingsCell", bundle: nil), forCellReuseIdentifier: "userSettingsCell") //register xib file to chat table view
     }
     
     @IBAction func backButton(_ sender: UIButton) {
         self.navigationController?.popViewController(animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "userSettingsCell", for: indexPath) as! UserSettingsCell //initiate custom cell for chat table view
+        //let username = ["TestUser"] //declared array for test data
+        cell.settingsLabel.text = "Log out" //alter chatUsername element with test data for username
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //self.navigationController?.popViewController(animated: true)
+        do {
+            try Auth.auth().signOut()
+            let controllerStack = self.navigationController?.viewControllers
+            self.navigationController?.popToViewController((controllerStack?[0])!, animated: true)
+        } catch let err {
+            print(err)
+        }
     }
 }
---
+++ Ping/View/Custom Cells/UserSettingsCell.swift
@@ -0,0 +1,25 @@
+//
+//  UserSettingsCell.swift
+//  Ping
+//
+//  Created by Ryan Soanes on 05/04/2019.
+//  Copyright © 2019 LionStone. All rights reserved.
+//
+
+import UIKit
+
+class UserSettingsCell: UITableViewCell {
+
+    @IBOutlet var settingsLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
