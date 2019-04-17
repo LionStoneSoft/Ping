e8718d1f3b4bd0d52ae1bbd34a3c21c3149cee67
Ryan Soanes <ryan@lionstone.app>
2019-04-17 14:51:26 +0100

    Rounded corners of messages for better UI

--- Ping/View/Custom Cells/CustomMessageCell.swift
+++ Ping/View/Custom Cells/CustomMessageCell.swift
@@ -17,12 +17,17 @@ class CustomMessageCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        roundedMessage()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    func roundedMessage() {
+        cellBackView.layer.cornerRadius = 10
     }
     
 }
--- Ping/View/Custom Cells/CustomMessageCell2.swift
+++ Ping/View/Custom Cells/CustomMessageCell2.swift
@@ -11,16 +11,22 @@ import UIKit
 class CustomMessageCell2: UITableViewCell {
 
     @IBOutlet var messageFromUserText: UILabel!
+    @IBOutlet var messageColourView: UIView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        roundedMessage()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    func roundedMessage() {
+        messageColourView.layer.cornerRadius = 10
     }
     
 }
